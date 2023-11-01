403979538f008fcc4ab1000e51e26433006bf3f4
Amirhossein Ayoubi <amirhosseinayoubi83@gmail.com>
2023-11-01 03:54:37 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+Console.WriteLine("chandomin donbale ro mikhi ?");
+int an =Convert.ToInt32(Console.ReadLine());
+
+int ShomareShekl = 1;
+for(int x = 0; x < an; x++)
+{
+    int number= (ShomareShekl * (ShomareShekl + 1)) / 2;
+    ShomareShekl = ShomareShekl + 1;
+    
+    Console.Write(number);
+    Console.Write(",");
+}
+
+
+
+
+
+
+
+
+
+
+
+
